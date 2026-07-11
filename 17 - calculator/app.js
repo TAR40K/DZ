1e7549158b058f02164d00b3e9800e76edde4404
TARCHOK <tarchok@MacBook-Air-TARCHOK.local>
2026-07-11 19:27:27 +0500

    calculator.fix3

--- 17 - calculator/app.js
+++ 17 - calculator/app.js
@@ -5,27 +5,47 @@ function calc() {
     btn.addEventListener(`click`, function () {
       const input1 = Number(document.querySelector(`#num1`).value);
       const input2 = Number(document.querySelector(`#num2`).value);
+      const result = document.querySelector(`#resultText`);
       const add = document.querySelector(`[data-action="add"]`);
       const subtract = document.querySelector(`[data-action="subtract"]`);
       const multiplay = document.querySelector(`[data-action="multiply"]`);
       const divide = document.querySelector(`[data-action="divide"]`);
+      const exponentiation = document.querySelector(
+        `[data-action="exponentiation"]`,
+      );
+      const clear = document.querySelector(`[data-action="clear"]`);
+      if (
+        document.querySelector(`#num1`).value === "" ||
+        document.querySelector(`#num2`).value === ""
+      ) {
+        result.textContent = `Заполни оба поля!`;
+        return;
+      }
       switch (btn) {
         case add:
-          document.querySelector(`#resultText`).textContent = input1 + input2;
+          result.textContent = input1 + input2;
           break;
         case subtract:
-          document.querySelector(`#resultText`).textContent = input1 - input2;
+          result.textContent = input1 - input2;
           break;
         case multiplay:
-          document.querySelector(`#resultText`).textContent = input1 * input2;
+          result.textContent = input1 * input2;
           break;
         case divide:
           if (input2 !== 0) {
-            document.querySelector(`#resultText`).textContent = input1 / input2;
+            result.textContent = input1 / input2;
             break;
           } else {
-            document.querySelector(`#resultText`).textContent = `Так нельзя`;
+            result.textContent = `Так нельзя`;
+            break;
           }
+        case exponentiation:
+          result.textContent = input1 ** input2;
+          break;
+        case clear:
+          document.querySelector(`#num1`).value = ``;
+          document.querySelector(`#num2`).value = ``;
+          break;
       }
       document.querySelector(`#num1`).value = ``;
       document.querySelector(`#num2`).value = ``;
