10d8b9aadf3aa71e13766cad8f13d445781e42d5
TARCHOK <tarchok@MacBook-Air-TARCHOK.local>
2026-07-24 23:43:04 +0500

    diceTry

---
+++ js.pro/2 - diceTry/app.js
@@ -0,0 +1,20 @@
+function diceTry(dice) {
+  const allDices = {
+    d4: 4,
+    d6: 6,
+    d8: 8,
+    d10: 10,
+    d12: 12,
+    d16: 16,
+    d20: 20,
+  };
+  const max = allDices[dice];
+  if (!max) {
+    return `Такого кубика нет`;
+  }
+  const result = Math.floor(Math.random() * max) + 1;
+  return result;
+}
+
+console.log(diceTry(`d6`));
+console.log(diceTry(`d20`));
